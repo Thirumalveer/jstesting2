012e8ac6c11bc4d88a2c553bbfed1f6faf976c92
Thirumalveer <thirumal21071994@gmail.com>
2024-06-20 00:53:30 +0530

    htmltasks

--- pattern_t5.js
+++ pattern_t5.js
@@ -1,6 +1,20 @@
 
 
 
+let arr = [1, 2, 3, 4, 5, 6];
+
+   for(i=0;i<=arr.length; i++){
+
+if(arr[i]%2==0){
+
+    let a = arr[i];
+     
+
+}
+   }
+
+
+
 for(i=0;i<=5;i++){
     str=" ";
     for(j=0;j<=i;j++){
---
+++ task5.js
@@ -0,0 +1,114 @@
+// 1.Write a program to find the sum of all even numbers in an array using a for loop.
+
+    // let arr = [1, 2, 3, 4, 5, 6];
+
+    // for(i=0;i<=arr.length;i++){
+
+    //    if(arr[i]%2==0){
+
+    //     console.log(arr[i])
+    //    }
+
+    // }
+
+// ans :2
+//      4
+//      6
+    // 2.Use a do-while loop to print the numbers from 1 to 10, but only if the number is not equal to 5.
+
+//     var str=+prompt("enter text here:")
+  
+//     let num=1;
+//  if(str !=5){
+//      do{
+//          console.log(num);
+//          num++
+//      }while(num<=10)
+//  }
+//  else{
+//      console.log("pls enter number except 5")
+//  }
+// ans:
+//  1
+//  2
+//  3
+//  4
+//  5
+//  6
+//  7
+//  8
+//  9
+//  10
+//  3.check whether given input string is palindrome or not?
+
+//   var str="mom";
+//      add=""
+   
+//      for(i=str.length-1;i>=0;i--){
+         
+//        add=add+str[i]
+//      }
+//      if(add==str){
+
+//         console.log("this is palindrome")
+//      }
+//      else
+//      console.log("this is not a palindrome")
+    
+//4.Write a program using a do-while loop to print the numbers from 10 to 1, but only if the number is odd. If the number is even, print "Skipping even number".
+
+// var str=+prompt("enter text here:")
+  
+// let num=10;
+// if(str%2==1){
+//  do{
+//      console.log(num);
+//      num--
+//  }while(num>=1)
+// }
+// else{
+//  console.log("Skipping even number")
+// }
+
+
+
+// 5.Write a program using a while loop to find the average of all numbers in an array. If the average is greater than 50, print "Average is large".
+
+
+// let arr=[1,2,3,4,5,6]
+// let avg=0
+//   let i=0
+// while(i<=arr.length-1){
+  
+//   avg=avg+arr[i];
+//   i++
+  
+// }
+
+// avg=avg/arr.length
+// if(avg==50){
+//     console.log("Average is large")
+// }
+// else
+// console.log("Average is small")
+
+
+// 6.pattern
+
+// for(i=0;i<=5;i++){
+//     str=" ";
+//     for(j=0;j<=i;j++){
+
+//     str +="*"
+//     }
+//     console.log(str)
+// }
+
+//  *
+//  **
+//  ***
+//  ****
+//  *****
+//  ******
+
+
